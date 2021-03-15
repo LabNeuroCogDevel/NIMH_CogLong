31d0f09e77348cf7d8cde7d5f43f67dde854e323
WillForan <willforan@gmail.com>
2021-03-15 11:05:05 -0400

    🐛 wasi not cbcl. but include cbcl anyway

--- WASI.R
+++ WASI.R
@@ -17,8 +17,8 @@ upload <- read.csv("txt/upload/T1_fmri_upload.csv", skip=1) %>%
 
 wasi <- db_query("
    select id, vtimestamp as wasi_date, age as wasi_age, measures
-   from visit_task natural join visit natural join enroll
-   where etype like 'LunaID' and task like 'CBCL'") %>%
+   from visit_task natural join visit join enroll on visit.pid = enroll.pid
+   where etype like 'LunaID' and task like 'WASI'") %>%
   unnestjson() %>%
   select(-matches("measures.[xy]")) %>%
   mutate(wasi_date=as.Date(wasi_date))
@@ -29,4 +29,18 @@ wasi_match <- date_match(upload, wasi, "id", "scan_date", "wasi_date", maxdatedi
 if(!dir.exists("txt/share")) dir.create("txt/share")
 write.csv(wasi_match, "txt/share/wasi_coglong.csv", row.names=F)
 
-# wasi_match %>% select(id, subjectkey, scan_date, scan_age, wasi_date, wasi_age, datediff.y, cbclTExternal, cbclTInternal) %>% tail
+# some CBCL was collected!
+cbcl <- db_query("
+   select id, vtimestamp as cbcl_date, age as cbcl_age, measures
+   from visit_task natural join visit join enroll on visit.pid = enroll.pid
+   where etype like 'LunaID' and task like 'CBCL'") %>%
+  unnestjson() %>%
+  select(-matches("measures.[xy]")) %>%
+  mutate(cbcl_date=as.Date(cbcl_date))
+
+cbcl_match <- date_match(upload, cbcl, "id", "scan_date", "cbcl_date", maxdatediff=Inf) %>%
+   relocate("datediff.y", .after="cbcl_age")
+# rows: 236/640. unique ids: 48/166
+write.csv(cbcl_match, "txt/share/cbcl_coglong.csv", row.names=F)
+
+# cbcl_match %>% select(id, subjectkey, scan_date, scan_age, wasi_date, wasi_age, datediff.y, )#cbclTExternal, cbclTInternal) %>% tail
